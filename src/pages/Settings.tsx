0cc515e55c343cd9cb37eac878cfc1ff511a3e79
YuraMakhnitko <bizoru82@gmail.com>
2023-05-05 15:54:16 +0300

    2

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,25 +1,59 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
+
 export const Settings: React.FC = () => {
+  const [value, setValue] = React.useState<number>(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
   return (
     <div className="todo-settings">
       <p className="todo-settings__text">
         User Name: <span>Rikel</span>
       </p>
+      <div className="todo-settings__devider"></div>
       <p className="todo-settings__text">
         User Email: <span>Rikel@rik.rik</span>
       </p>
+      <div className="todo-settings__devider"></div>
+
       <div className="todo-settings__language-box">
         <label htmlFor="" className="todo-settings__label">
-          Language
+          Language:
         </label>
         <input type="radio" />
         <input type="radio" />
       </div>
-      <div>
+      <div className="todo-settings__devider"></div>
+
+      <div className="todo-settings__sounds-box">
         <label htmlFor="" className="todo-settings__label">
-          Sounds
+          Sounds:
         </label>
-        <input type="radio" />
+        <Box sx={{ width: 200 }}>
+          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+            <VolumeDown sx={{ opacity: 0.7 }} />
+            <Slider
+              aria-label="Volume"
+              value={value}
+              onChange={handleChange}
+              // sx={{ opacity: 0.7 }}
+              // sx={{ color: "rgba(8, 80, 100, 1)" }}
+              sx={{ color: "rgba(97, 218, 251, 1)" }}
+            />
+            <VolumeUp sx={{ opacity: 0.7 }} />
+          </Stack>
+        </Box>
       </div>
+      <div className="todo-settings__devider"></div>
+
       <button className="todo__button-logout">Logout</button>
     </div>
   );
